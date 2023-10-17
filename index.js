a53c84a6af4850b2a4609750d99863c00452533e
zainabmd <izainabmahmood@gmail.com>
2023-10-17 20:52:34 +0500

    added delete all button to clear LS, DOM & myLeads

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ let myLeads=[];
 const inputF=document.getElementById("input-field")
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul");
+const delBtn=document.getElementById("delete-btn");
 
-let LSLeads=JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    renderLeads();
+}
+//clearing localStorage, myLeads and DOM when DELETE ALL button
+delBtn.addEventListener("dblclick",function(){
+    localStorage.clear();
+    myLeads=[];
+    renderLeads();
+})
 
 inputBtn.addEventListener('click',function(){
     myLeads.push(inputF.value);
     inputF.value="";
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
     renderLeads();
-
+    console.log(localStorage.getItem("myLeads"));
 })
 function renderLeads(){
     let listItems="";
