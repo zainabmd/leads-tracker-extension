4672afb8fa5fe2af57d9e082dd37dfbb35a0ab7a
zainabmd <izainabmahmood@gmail.com>
2023-10-17 22:29:22 +0500

    save current tab button to get active tab's url

--- index.js
+++ index.js
@@ -4,42 +4,49 @@ const inputF=document.getElementById("input-field")
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul");
 const delBtn=document.getElementById("delete-btn");
+const tabBtn=document.getElementById("savetab-btn")
 
 const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem (JSON.stringify(myLeads) )
+        render(myLeads);
+    })
+})
 
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
+}
+function render(leads){
+    let listItems="";
+
+    for(let i=0;i<leads.length;i++){
+        listItems+=
+        `<li>
+            <a target='_blank' href="${leads[i]}"/>
+            ${leads[i]}
+        </li>`;
+        /* 2nd/long way to do above line
+        const li=document.createElement("li");
+        li.textContent+=myLeads[i];
+        ulEl.append(li); */
+
+    }
+    ulEl.innerHTML=listItems;
 }
 //clearing localStorage, myLeads and DOM when DELETE ALL button
 delBtn.addEventListener("dblclick",function(){
     localStorage.clear();
     myLeads=[];
-    renderLeads();
+    render(myLeads);
 })
 
 inputBtn.addEventListener('click',function(){
     myLeads.push(inputF.value);
     inputF.value="";
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderLeads();
-    console.log(localStorage.getItem("myLeads"));
+    render(myLeads);
 })
-function renderLeads(){
-    let listItems="";
-
-for(let i=0;i<myLeads.length;i++){
-listItems+=
-    `<li>
-        <a target='_blank' href="${myLeads[i]}"/>
-        ${myLeads[i]}
-    </li>`;
-/* 2nd/long way to do above line
-const li=document.createElement("li");
-li.textContent+=myLeads[i];
-ulEl.append(li); */
-
-}
-ulEl.innerHTML=listItems;
-}
